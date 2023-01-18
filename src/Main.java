613fdd3bf618e6227ee38c4bac635c37490d90d2
Zununova <121685002+Zununova@users.noreply.github.com>
2023-01-18 18:43:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss(1000, 50);
-        System.out.println(boss.getHealth()+" "+ boss.getDamage()+" "+ boss.getWeapon();
+        System.out.println(boss.getHealth()+" "+ boss.getDamage()+" "+ boss.getWeapon());
     }
 }
